3a1af237099eb166295d2296926962c2f35e632d
Josh <jemidlo17@gmail.com>
2020-09-29 17:33:48 -0500

    updated assignment

---
+++ Fundamentals/exceptions/exception.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class exception {
+
+    public static void main(String[] args) {
+        ArrayList<Object> myList = new ArrayList<Object>();
+        myList.add("13");
+        myList.add("hello world");
+        myList.add(48);
+        myList.add("Goodbye World");
+
+        for (int i = 0; i < myList.size(); i++) {
+            try {
+                Integer castedValue = (Integer) myList.get(i);
+                System.out.println(castedValue);
+            }
+            catch(Exception e) {
+                System.out.println("Something went wrong");
+                System.out.println(e);
+            }
+        }
+    }
+
+
+
+}
